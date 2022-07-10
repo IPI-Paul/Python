6fec687ea910ecc268491192fccece1f80a8d69c
Paul I Ighofose <paul@ipi-international.co.uk>
2022-07-10 15:03:43 +0100

    Added Rich Text Editor
    Text to Speech - Clipboard reader for Samsung Galaxy Tab S3

--- IPI Galaxy Tab Text to Speech/IPI Galaxy Tab Text to Speech.py
+++ IPI Galaxy Tab Text to Speech/IPI Galaxy Tab Text to Speech.py
@@ -132,14 +132,14 @@ class TTSWindow(QMainWindow, QApplication):
     	# Language in which you want to convert
         language = 'en'
 			
-				# Passing the text and language to the engine,
-				# here we have marked slow=False. Which tells
-				# the module that the converted audio should
-				# have a high speed
+	# Passing the text and language to the engine,
+	# here we have marked slow=False. Which tells
+	# the module that the converted audio should
+	# have a high speed
         myobj = gTTS(text=mytext, lang=language, slow=False)
 			
-				# Saving the converted audio in a mp3 file named
-				# welcome
+	# Saving the converted audio in a mp3 file named
+	# welcome
         myobj.save("welcome.mp3")
 									
 if __name__ == "__main__":
